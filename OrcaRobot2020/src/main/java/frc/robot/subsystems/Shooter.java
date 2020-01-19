815a9c3263cbc54a030065afa876aeded583a27e
HESAIL <lukesale23@gmail.com>
2020-01-18 17:37:30 -0800

    Added Subsystems for all solenoids and motors. Added subsyestem for shooting procedure, This is incomplete, untested code.

---
+++ OrcaRobot2020/src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+// import java.util.Map;
+
+
+
+// import edu.wpi.first.networktables.NetworkTableEntry;
+// import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+// import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+// import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+public class Shooter extends SubsystemBase {
+    // Motor Controllers: 
+  public void shoot (){
+  //flywheel m_fly = new flywheel();
+  //ConveySoli m_ConSol = new ConveySoli();
+  }
+}
